b7cf0f1b39cf0aa64b20cfb0b100e751fcf88138
curiousleo <curiousleo@users.noreply.github.com>
2021-11-11 00:22:02 +0100

    Fix upper bound

--- generate-ocaml/generate.ml
+++ generate-ocaml/generate.ml
@@ -2,7 +2,7 @@ open Base
 open Stdio
 
 let () =
-  let bound: Int64.t = 0xFFFFFFFFL in
+  let bound: Int64.t = 0x100000000L in
   while true do
     let r = Random.int64 bound in
     printf "%08Lx\n" r
